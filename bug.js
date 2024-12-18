5cfd29f093e08d1f04967a7ff0a23462d6d210bf
BugHunter-X <askzenai@gmail.com>
2024-12-18 11:40:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gray-300 p-4 rounded-lg">
+  <h2 class="text-xl font-bold mb-2">Unexpected Behavior</h2>
+  <p>This is some text with a class that is causing an unexpected rendering issue in Tailwind CSS.</p>
+</div>
+```
